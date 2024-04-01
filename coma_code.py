dc18c646a7b715eaef5fd9ff4f7b0f79628d50bf
Jaime Escobedo <maildeljimmy@gmail.com>
2024-03-31 23:07:30 -0600

    Coma code fix data types

--- coma_code.py
+++ coma_code.py
@@ -4,14 +4,15 @@ def join_list(items_list):
     elif len(items_list) == 1:
         return ' '.join(items_list)
     else:    
-        last_item = items_list.pop()
-        final_str = f"{', '.join(items_list)} and {last_item}"
+        last_item = str(items_list.pop())
+        items_list_str = [str(i) for i in items_list]
+        final_str = f"{', '.join(items_list_str)} and {last_item}"
         return  final_str
 
 
 
 
-spam = ['apples', 'bananas', 'tofu', 'cats']
+spam = ['apples', 'bananas', 'tofu', 'cats', True, 13]
 
 prueba = join_list(spam)
 print(prueba)
